25f8e37c0468e226f19031b9a70f93f6fb9ecd60
Yao Wang <yw438@cornell.edu>
2015-03-09 15:37:39 -0400

    add some identity proof, not sure about how to prove behavior

--- Circuits.v
+++ Circuits.v
@@ -11,7 +11,34 @@ Import VectorNotations.
 Require Import Plus.
 Require Import Arith.
 
+Require String. Open Scope string_scope.
 
+Ltac move_to_top x :=
+  match reverse goal with
+  | H : _ |- _ => try move x after H
+  end.
+
+Tactic Notation "assert_eq" ident(x) constr(v) :=
+  let H := fresh in
+  assert (x = v) as H by reflexivity;
+  clear H.
+
+Tactic Notation "Case_aux" ident(x) constr(name) :=
+  first [
+    set (x := name); move_to_top x
+  | assert_eq x name; move_to_top x
+  | fail 1 "because we are working on a different case" ].
+
+Tactic Notation "Case" constr(name) := Case_aux Case name.
+Tactic Notation "SCase" constr(name) := Case_aux SCase name.
+Tactic Notation "SSCase" constr(name) := Case_aux SSCase name.
+Tactic Notation "SSSCase" constr(name) := Case_aux SSSCase name.
+Tactic Notation "SSSSCase" constr(name) := Case_aux SSSSCase name.
+Tactic Notation "SSSSSCase" constr(name) := Case_aux SSSSSCase name.
+Tactic Notation "SSSSSSCase" constr(name) := Case_aux SSSSSSCase name.
+Tactic Notation "SSSSSSSCase" constr(name) := Case_aux SSSSSSSCase name.
+(** Here's an example of how [Case] is used.  Step through the
+   following proof and observe how the context changes. *)
 
 (* ~~~~~~~ DEFINITIONS ~~~~~~~ *)
 
@@ -197,37 +224,82 @@ Fixpoint par_wire (n : nat) : Circuit n n :=
     | S n' => par wire (par_wire n')
   end.
 
+Theorem area_par_wire : forall (n : nat),
+  area (par_wire n) = 0.
+Proof. 
+  intros n.
+  induction n as [|n'].
+  Case "n = 0". reflexivity.
+  Case "n = S n'". simpl. apply IHn'.
+Qed.
+
 Theorem area_comp_ident_l : forall (m n : nat) (A : Circuit m n),
   area (comp (par_wire m) A) = area A.
-Proof. Admitted.
+Proof. 
+  intros m n A. 
+  simpl. rewrite -> area_par_wire. reflexivity.
+Qed.
 
 Theorem area_comp_ident_r : forall (m n : nat) (A : Circuit m n),
   area (comp A (par_wire n)) = area A.
-Proof. Admitted.
+Proof.
+  intros m n A.
+  simpl. rewrite -> area_par_wire.
+  rewrite -> plus_comm. reflexivity.
+Qed.
 
 Theorem area_par_ident_l : forall (m n : nat) (A : Circuit m n),
   area (par none A) = area A.
-Proof. Admitted.
+Proof. 
+  intros m n A.
+  reflexivity.
+Qed.
 
 Theorem area_par_ident_r : forall (m n : nat) (A : Circuit m n),
   area (par A none) = area A.
-Proof. Admitted.
+Proof. 
+  intros m n A.
+  simpl.  rewrite -> plus_comm.
+  reflexivity.
+Qed.
+
+Theorem delay_par_wire : forall (n : nat),
+  delay (par_wire n) = 0.
+Proof.
+  intros n.
+  induction n as [|n'].
+  Case "n = 0". reflexivity.
+  Case "n = S n'". simpl. apply IHn'.
+Qed.
 
 Theorem delay_comp_ident_l : forall (m n : nat) (A : Circuit m n),
   delay (comp (par_wire m) A) = delay A.
-Proof. Admitted.
+Proof. 
+  intros m n A.
+  simpl. rewrite -> delay_par_wire. reflexivity.
+Qed.
 
 Theorem delay_comp_ident_r : forall (m n : nat) (A : Circuit m n),
   delay (comp A (par_wire n)) = delay A.
-Proof. Admitted.
+Proof. 
+  intros m n A.
+  simpl. rewrite -> delay_par_wire.
+  rewrite -> plus_comm.  reflexivity.
+Qed.
 
 Theorem delay_par_ident_l : forall (m n : nat) (A : Circuit m n),
   delay (par none A) = delay A.
-Proof. Admitted.
+Proof.
+  intros m n A.
+  reflexivity.
+Qed.
 
 Theorem delay_par_ident_r : forall (m n : nat) (A : Circuit m n),
   delay (par A none) = delay A.
-Proof. Admitted.
+Proof. 
+  intros m n A.
+  simpl. apply max_l. 
+  Admitted.
 
 Theorem behavior_comp_ident_l : forall (m n : nat) (A : Circuit m n),
   behavior (comp (par_wire m) A) = behavior A.
